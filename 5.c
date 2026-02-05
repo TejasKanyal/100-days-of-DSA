2e6db19af08a84a4c7e825a630f818be27b8a0ef
Tejas Kanyal <tejaskanyal2006@gmail.com>
2026-02-05 09:51:14 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int a[100];
+    int n, k, i;
+    int count = 0;
+    int found = 0;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &k);
+
+    for(i = 0; i < n; i++) {
+        count++;             
+        if(a[i] == k) {
+            printf("Found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0) {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d", count);
+
+    return 0;
+}
